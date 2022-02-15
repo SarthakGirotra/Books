330a3a1f61e20dacaf32994c4e1abd502bfd59c3
SarthakGirotra <sarthakgirotra@gmail.com>
2022-02-15 23:21:39 +0530

    v2

--- frontend/src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux'
-import Auth from './user'
-import books from "./books"
-import error from "./error"
-export default combineReducers({ Auth, books, error })
--- user-microservice/models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type User struct {
-	ID       string `json:"id,omitempty"  bson:"_id,omitempty" example:"462784683264287647223"`
-	Email    string `json:"email" validate:"required,email" example:"sarthak@gmail.com"`
-	Password string `json:"password" validate:"pass" example:"Test@1234000"`
-}
